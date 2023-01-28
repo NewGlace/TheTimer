9b1903107557ef68de867feadd7e284d60df22eb
NewGlace <NewGlace@outlook.fr>
2023-01-29 00:08:50 +0100

    update test

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { classicTimer } from "./utils/func/classicTimer";
 import { newTimer } from "./utils/func/newTimer";
 
-const count = 100000;
-const timer = 1000
-switch(<Boolean>false) {
+const count = 1;
+const timer = 100
+const func = () => {
+    for (let i = 0; i < 100; i++) {
+        String(Date.now());
+        String(Date.now());
+    }
+}
+
+switch(<Boolean>true) {
     case true: 
-        newTimer(count, timer)
+        newTimer(count, timer, func)
     break
     case false: 
-        classicTimer(count, timer)
+        classicTimer(count, timer, func)
     break;
 }
